cc77e06e192c6e2fdd0ce2508eb6d946f2af0e89
Arthur Stofeles <arthur.stofeles.web@gmail.com>
2020-07-20 11:09:56 -0300

    Add Validate Styles Settings

--- script.js
+++ script.js
@@ -9,13 +9,3 @@ function validateEmail(){
         mensage.innerHTML = email.validationMessage;
     }
 };
-
-// function myFunction() {
-//     var inpObj = document.getElementById("id1");
-//     if (!inpObj.checkValidity()) {
-//       document.getElementById("demo").innerHTML = inpObj.validationMessage;
-//     } else {
-//       document.getElementById("demo").innerHTML = "Input OK";
-//     } 
-//   } 
-
